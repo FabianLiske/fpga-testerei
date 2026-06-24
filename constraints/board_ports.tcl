f72ed86ca4c2a018e618f25048bf3669e674b6a0
Fabian Liske 🚀 <19373177+FabianLiske@users.noreply.github.com>
2026-06-24 23:26:39 +0200

    NIC LEDs abwechseln blinken

--- constraints/board_ports.tcl
+++ constraints/board_ports.tcl
@@ -6,9 +6,17 @@
 proc emit_project_board_constraints {fh pins c emitted_ports_var} {
     upvar $emitted_ports_var emitted_ports
 
+    set board_xml [dict get $c board_xml]
+    set clk_freq [board_clock_frequency $board_xml diff_100mhz_clk 100000000]
+    set clk_period [format %.3f [expr {1000000000.0 / double($clk_freq)}]]
     set led_iostandard [dict get $c led_iostandard]
 
-    puts $fh "## Project mapping: red user LED."
-    puts $fh "## LED_IOSTANDARD=$led_iostandard; use LED_IOSTANDARD=NONE to omit it or override with e.g. LVCMOS33."
-    emit_optional_board_port_constraint $fh $pins GPIO_LED_R led $led_iostandard emitted_ports
+    emit_optional_board_port_constraint $fh $pins diff_100mhz_clk_p clk_100mhz_p BOARD emitted_ports
+    emit_optional_board_port_constraint $fh $pins diff_100mhz_clk_n clk_100mhz_n BOARD emitted_ports
+    emit_optional_clock_constraint $fh clk_100mhz_p $clk_period clk_100mhz
+
+    #puts $fh "## Project mapping: red user LED."
+    #puts $fh "## LED_IOSTANDARD=$led_iostandard; use LED_IOSTANDARD=NONE to omit it or override with e.g. LVCMOS33."
+    emit_optional_board_port_constraint $fh $pins SFP_1_LED {led[0]} $led_iostandard emitted_ports
+    emit_optional_board_port_constraint $fh $pins SFP_2_LED {led[1]} $led_iostandard emitted_ports
 }
--- rtl/top.sv
+++ rtl/top.sv
@@ -1,9 +1,32 @@
 `timescale 1ns/1ps
 
 module top (
-    output wire led
+    input  wire       clk_100mhz_p,
+    input  wire       clk_100mhz_n,
+    output wire [1:0] led
 );
+    wire clk_ibuf;
+    wire clk;
+    logic [26:0] counter = '0;
 
-    assign led = 1'b1;
+    IBUFDS #(
+        .DIFF_TERM("TRUE"),
+        .IOSTANDARD("LVDS")
+    ) u_clk_ibufds (
+        .I(clk_100mhz_p),
+        .IB(clk_100mhz_n),
+        .O(clk_ibuf)
+    );
+
+    BUFG u_clk_bufg (
+        .I(clk_ibuf),
+        .O(clk)
+    );
+
+    always_ff @(posedge clk) begin
+        counter <= counter + 1'b1;
+    end
+
+    assign led = counter[26] ? ~2'b01 : ~2'b10;
 
 endmodule
